582bc5ae422df21c49236d860b0c046330d4e1e1
sairaj totre <123862597+SaitajTotre@users.noreply.github.com>
2023-04-10 22:10:05 +0530

    ok done

---
+++ Switch/src/com/swi/Checkevenorodd.java
@@ -0,0 +1,15 @@
+package com.swi;
+
+public class Checkevenorodd {
+	public static void main (String args[]){
+		int num1=100;
+		int num2=111;
+		switch(num1%2){//this will return 0
+		case 0:
+		    System.out.println(num1+" is an Even number");
+		    break;
+		case 1:
+		    System.out.println(num2+" is an Odd number");
+		}
+		}
+}
