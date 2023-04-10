3b0b1771bed77d5eaeee7ae4291259b36e878066
sairaj totre <123862597+SaitajTotre@users.noreply.github.com>
2023-04-10 15:07:41 +0530

    ok done

---
+++ Greatestno.java
@@ -0,0 +1,32 @@
+package com.swi;
+import java.util.Scanner;
+public class Greatestno {
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Enter the Number 1 : ");
+		int n1 = input.nextInt();
+		System.out.printf("Enter the Number 2 : ");
+		int n2 = input.nextInt();
+		int res = n1>n2?1:0;
+		switch (res)
+		{
+			case 0:				
+				res = n1==n2?1:0;
+				switch(res)
+				{
+					case 0:						
+						System.out.printf("Maximum Number is "+n2);
+						break;					
+					case 1:						
+						System.out.printf("Both are Equals");
+						break;
+				}
+				break;
+ 
+			case 1:
+				System.out.printf("Maximum Number is "+n1);
+				break;
+		}
+	}
+}
